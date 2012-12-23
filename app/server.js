425d8ca4bbf4d5ed695058bd890ecd36ad66bd57
Bill Jones <bj7857@gmail.com>
2012-12-23 12:06:24 -0500

    Incorperate the forever cookbook to keep the server running and watch the directory for changes.

--- app/server.js
+++ app/server.js
@@ -5,6 +5,7 @@ var port = process.env.PORT || 8888;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World with forever 2\n');
 }).listen(port, ip);
+
 console.log('Server running at %s:%d', ip, port);
--- app/startServer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is used to start the application from vagrant
-echo "Starting node application"
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SCRIPT=$DIR"/server.js"
-echo Executing Script $SCRIPT
-node $SCRIPT 
